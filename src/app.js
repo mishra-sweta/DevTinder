8d858abbd9453df0627daf4c3a0fd721f5d235ef
mishra-sweta <swetamishra.liluah@gmail.com>
2025-05-08 16:33:48 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,16 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from the server!");
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Response 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Response 2");
+    res.send("Response!!!!!!!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Response 3");
+    next();
+  }
+);
 
-app.post("/test", (req, res) => {
-  res.send("Hello from test!");
-});
-
-app.get("/hello", (req, res) => {
-  res.send("Hello hello hello");
+app.get("/user", (req, res) => {
+  console.log("Response 4");
 });
 
 app.listen(3000, () => {
